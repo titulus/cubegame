704554863d2e15f8be06c2e110846bf544baf34c
titulus <titulus.web@gmail.com>
2014-05-25 18:12:53 +0300

    do OOP

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var cube3d = document.getElementById('cube3d');
-var sides = ['front','top','bottom','back','left','right'];
+function Cube (element_id) {
+	var _DOM = document.getElementById(element_id);
+	this.DOM = _DOM;
 
-var cube = {} // main cube object
-	for (i in sides) {cube[sides[i]]=['z','-y','y','-z','-x','x'][i]}
+	var sides = ['front','top','bottom','back','left','right'];
+
+	for (i in sides) {this[sides[i]]=['z','-y','y','-z','-x','x'][i]}
+
+	return this;
+}
+
+var cube = new Cube('cube3d') // main cube object	
