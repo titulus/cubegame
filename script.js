194940711426e00ce4be891e93162449b39a1c3a
titulus <titulus.web@gmail.com>
2014-05-25 17:55:02 +0300

    fill cube object with sides and they ords

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var cube3d = document.getElementById('cube3d');
 var sides = ['front','top','bottom','back','left','right'];
 
 var cube = {} // main cube object
+	for (i in sides) {cube[sides[i]]=['z','-y','y','-z','-x','x'][i]}
