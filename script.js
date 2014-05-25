152a0ba7c7f83896f8c69f7c6a12387e1ff8accc
titulus <titulus.web@gmail.com>
2014-05-25 18:21:08 +0300

    add fill method. which fill DOM cube sides with appropriate values

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function Cube (element_id) {
-	var _DOM = document.getElementById(element_id);
-	this.DOM = _DOM;
+	var DOM = document.getElementById(element_id);
+	this.DOM = DOM;
 
 	var sides = ['front','top','bottom','back','left','right'];
 
 	for (i in sides) {this[sides[i]]=['z','-y','y','-z','-x','x'][i]}
 
+	function fill () { // fill DOM cube sides with appropriate values
+		for (i in sides) {
+			var element = document.getElementById(sides[i]);
+			var value = this[sides[i]];
+			element.innerHTML=value;
+		}
+	}
+	this.fill = fill;
+
 	return this;
 }
 
 var cube = new Cube('cube3d') // main cube object	
+cube.fill();
