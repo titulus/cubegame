6f31b64cdcbd0cde1bfd443b8dd50c9ac6239aa9
titulus <tituilus.web@gmail.com>
2025-01-13 19:52:53 +0300

    make index.html GA to be env specific v2

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,42 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/\.(mp3|wav)$/i.test(assetInfo.name)) {
-            return 'sounds/[name][extname]'
-          }
-          if (/\.(png|jpe?g|gif|svg|webp)$/i.test(assetInfo.name)) {
-            return 'img/[name][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html')
         },
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
+        output: {
+          assetFileNames: (assetInfo) => {
+            const info = assetInfo.name.split('.')
+            const ext = info[info.length - 1]
+            if (/\.(mp3|wav)$/i.test(assetInfo.name)) {
+              return 'sounds/[name][extname]'
+            }
+            if (/\.(png|jpe?g|gif|svg|webp)$/i.test(assetInfo.name)) {
+              return 'img/[name][extname]'
+            }
+            return 'assets/[name]-[hash][extname]'
+          },
+          chunkFileNames: 'assets/[name]-[hash].js',
+          entryFileNames: 'assets/[name]-[hash].js'
+        }
       }
-    }
-  },
-  publicDir: 'public',
-  base: '/',
-  assetsInclude: ['**/*.gif', '**/*.png', '**/*.jpg', '**/*.mp3', '**/*.js']
+    },
+    publicDir: 'public',
+    base: '/',
+    assetsInclude: ['**/*.gif', '**/*.png', '**/*.jpg', '**/*.mp3', '**/*.js'],
+    
+    plugins: [{
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/%GA_MEASUREMENT_ID%/g, env.GA_MEASUREMENT_ID || '');
+      }
+    }]
+  }
 })
